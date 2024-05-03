1337f957664cf5257c71e167af489b29e53d52d2
Angeles Guyot <angeles.guyot@kellogg.northwestern.edu>
2024-05-03 20:34:51 +0000

    jj

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   resources "tacos"
   resources "dice" #este es el primer paso que escribo para poder crear la pagina
+  resources "cards" 
 end
 
 #voy agregando /tacos a la pagina de rails para ver si anda
