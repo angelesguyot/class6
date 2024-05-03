0525b004c359ff87c80e076e4905989a72180ce2
Angeles Guyot <angeles.guyot@kellogg.northwestern.edu>
2024-05-03 19:51:57 +0000

    guardar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
+  resources "tacos"
 end
+
+#voy agregando /tacos a la pagina de rails para ver si anda
+# si pongo control c se me deja de correr lo que tengo en el cuadrado de abajo y vuelvo al coso comun
+#rails generate controller tacos
